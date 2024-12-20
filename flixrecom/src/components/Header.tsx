3f61fc73f5bc1f96d0ae8fe5a9745eb0ff8c183b
antonio santos <antoniosantos.devweb@gmail.com>
2024-12-20 08:07:39 -0300

    changes

--- flixrecom/src/components/Header.tsx
+++ flixrecom/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import { FaSearch, FaStar } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 
 interface Movie {
   id: number;
