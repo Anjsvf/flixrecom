b22a5e36087b219830dba08aaedecfcec2d62fb0
antonio santos <antoniosantos.devweb@gmail.com>
2024-12-20 07:48:14 -0300

    bugs  fixed

--- flixrecom/src/app/page.tsx
+++ flixrecom/src/app/page.tsx
@@ -1,27 +1,27 @@
 
 "use client"
 
-import { useState } from "react";
+// import { useState } from "react";
 import Header from "@/components/Header";
 import Banner from "@/components/Banner";
 import MovieRow from "@/components/MovieRow";
 import FilterMenu from "@/components/FilterMenu";
 
 export default function Home() {
-  const [category, setCategory] = useState("movies");
+  // const [category, setCategory] = useState("movies");
 
-  const requests = {
-    movies: `${process.env.NEXT_PUBLIC_TMDB_BASE_URL}/discover/movie`,
-    series: `${process.env.NEXT_PUBLIC_TMDB_BASE_URL}/discover/tv`,
-    documentaries: `${process.env.NEXT_PUBLIC_TMDB_BASE_URL}/discover/movie?with_genres=99`,
-  };
+  // const requests = {
+  //   movies: `${process.env.NEXT_PUBLIC_TMDB_BASE_URL}/discover/movie`,
+  //   series: `${process.env.NEXT_PUBLIC_TMDB_BASE_URL}/discover/tv`,
+  //   documentaries: `${process.env.NEXT_PUBLIC_TMDB_BASE_URL}/discover/movie?with_genres=99`,
+  // };
 
   return (
     <div className="bg-black text-white min-h-screen">
       <Header />
       <Banner />
       <div className="container mx-auto px-4">
-        <FilterMenu setCategory={setCategory} />
+        {/* <FilterMenu setCategory={setCategory} /> */}
         <MovieRow
          
         />
