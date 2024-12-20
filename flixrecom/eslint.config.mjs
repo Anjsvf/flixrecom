442b68416b0f43f12af74d7a85954cf41c16018d
antonio santos <antoniosantos.devweb@gmail.com>
2024-12-19 21:00:14 -0300

    changes on slint

--- flixrecom/eslint.config.mjs
+++ flixrecom/eslint.config.mjs
@@ -2,19 +2,15 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-// Obter o diretório e o nome do arquivo atual
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Inicializar o compat com o diretório base
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-// Configuração ESLint com os "extends" corretos
-const eslintConfig = compat.extends([
-  "next/core-web-vitals",  // Configurações do Next.js para performance e acessibilidade
-  "next/typescript"        // Configurações do Next.js para TypeScript
-]);
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
 
 export default eslintConfig;
