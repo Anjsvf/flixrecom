94761c73c0ffe029d93051a808518919ce9dd372
antonio santos <antoniosantos.devweb@gmail.com>
2024-12-20 13:48:44 -0300

    types

--- flixrecom/types/Movie.ts
+++ flixrecom/types/Movie.ts
@@ -1,11 +1,11 @@
-export interface Movie {
-    id: number;
-    title: string;
-    name?: string;
-    overview: string;
-    poster_path: string;
-    backdrop_path: string;
-    vote_average: number;
-    release_date: string;
-  }
+// export interface Movie {
+//     id: number;
+//     title: string;
+//     name?: string;
+//     overview: string;
+//     poster_path: string;
+//     backdrop_path: string;
+//     vote_average: number;
+//     release_date: string;
+//   }
   
