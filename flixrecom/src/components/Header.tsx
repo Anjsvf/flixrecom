05cfdfb0afec6cb1a5abe603a5cf85f78c66a8ab
antonio santos <antoniosantos.devweb@gmail.com>
2025-01-01 15:41:50 -0300

    log error addd

--- flixrecom/src/components/Header.tsx
+++ flixrecom/src/components/Header.tsx
@@ -42,8 +42,9 @@ export default function Header() {
       );
       const video = response.data.items[0];
       setTrailerId(video.id.videoId);
-    } catch {
+    } catch (error) {
       setErrorMessage("Erro ao buscar trailer no YouTube.");
+      console.error("Erro ao buscar trailer no YouTube:", error);
     }
   };
 
@@ -55,7 +56,10 @@ export default function Header() {
 
     setErrorMessage("");
 
-    if (!process.env.NEXT_PUBLIC_TMDB_API_KEY || !process.env.NEXT_PUBLIC_TMDB_BASE_URL) {
+    if (
+      !process.env.NEXT_PUBLIC_TMDB_API_KEY ||
+      !process.env.NEXT_PUBLIC_TMDB_BASE_URL
+    ) {
       setErrorMessage("Configuração da API inválida.");
       return;
     }
@@ -168,7 +172,6 @@ export default function Header() {
                 Limpar
               </button>
             </div>
-           
           </nav>
         </div>
       </header>
@@ -179,119 +182,163 @@ export default function Header() {
         </div>
       )}
 
-{suggestions.length > 0 && query && (
-  <div className="absolute bg-gray-700 w-full mt-16 sm:mt-20 md:mt-24 rounded-lg shadow-lg z-50"> {/* Margem maior e responsiva */}
-    <ul className="text-white divide-y divide-gray-600">
-      {suggestions.slice(0, 5).map((movie) => (
-        <li
-          key={movie.id}
-          className="p-2 hover:bg-gray-600 cursor-pointer transition duration-200 ease-in-out"
-          onClick={() => {
-            setQuery(movie.title || movie.name || "");
-            handleSearch();
-            setSuggestions([]);
-          }}
-        >
-          <div className="flex items-center space-x-2">
-            <div className="w-10 h-10 sm:w-12 sm:h-12 overflow-hidden rounded-md"> {/* Imagem responsiva */}
-              <img
-                src={
-                  movie.backdrop_path
-                    ? `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`
-                    : "/placeholder.jpg"
-                }
-                alt={movie.title || movie.name || "No title available"}
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <span className="text-sm sm:text-base lg:text-lg">{movie.title || movie.name}</span> {/* Texto responsivo */}
-          </div>
-        </li>
-      ))}
-    </ul>
-  </div>
-)}
-
+      {suggestions.length > 0 && query && (
+        <div className="absolute bg-gray-700 w-full mt-16 sm:mt-20 md:mt-24 rounded-lg shadow-lg z-50">
+          {" "}
+          {/* Margem maior e responsiva */}
+          <ul className="text-white divide-y divide-gray-600">
+            {suggestions.slice(0, 5).map((movie) => (
+              <li
+                key={movie.id}
+                className="p-2 hover:bg-gray-600 cursor-pointer transition duration-200 ease-in-out"
+                onClick={() => {
+                  setQuery(movie.title || movie.name || "");
+                  handleSearch();
+                  setSuggestions([]);
+                }}
+              >
+                <div className="flex items-center space-x-2">
+                  <div className="w-10 h-10 sm:w-12 sm:h-12 overflow-hidden rounded-md">
+                    {" "}
+                    {/* Imagem responsiva */}
+                    <img
+                      src={
+                        movie.backdrop_path
+                          ? `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`
+                          : "/placeholder.jpg"
+                      }
+                      alt={movie.title || movie.name || "No title available"}
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                  <span className="text-sm sm:text-base lg:text-lg">
+                    {movie.title || movie.name}
+                  </span>{" "}
+                  {/* Texto responsivo */}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
 
       {searchResults.length > 0 && (
         <div className="container mx-auto mt-20 p-4">
-          <h2 className="text-xl font-bold text-white">Resultados da Pesquisa:</h2>
+          <h2 className="text-xl font-bold text-white">
+            Resultados da Pesquisa:
+          </h2>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-4">
             {searchResults.map((movie) => (
               <div
                 key={movie.id}
                 className="bg-gray-800 p-2 rounded cursor-pointer"
-                onClick={() => fetchMovieDetails(movie.id, !!movie.first_air_date)}
+                onClick={() =>
+                  fetchMovieDetails(movie.id, !!movie.first_air_date)
+                }
               >
                 <img
-                  src={movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : "/placeholder.jpg"}
+                  src={
+                    movie.poster_path
+                      ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                      : "/placeholder.jpg"
+                  }
                   alt={movie.title || movie.name || "Imagem não disponível"}
                   className="w-full h-[300px] object-cover rounded"
                 />
-                <p className="text-white mt-2 text-sm">{movie.title || movie.name}</p>
+                <p className="text-white mt-2 text-sm">
+                  {movie.title || movie.name}
+                </p>
               </div>
             ))}
           </div>
         </div>
       )}
 
-{selectedMovie && (
-  <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center overflow-y-auto z-50">
-    <div className="bg-gray-900 p-4 rounded-md max-w-4xl mx-auto relative overflow-y-auto flex flex-col md:flex-row md:gap-6">
-      <button
-        className="absolute top-3 right-3 text-white bg-red-500 px-3 py-1 text-sm rounded"
-        onClick={() => setSelectedMovie(null)}
-      >
-        Fechar
-      </button>
-      <div className="flex-1">
-        <h2 className="text-xl font-bold text-white mb-3">{selectedMovie.title || selectedMovie.name}</h2>
-        <p className="text-gray-400 text-sm italic mb-2">
-          Tipo: {selectedMovie.first_air_date ? "Série" : "Filme"}
-        </p>
-        <img
-          src={selectedMovie.backdrop_path ? `https://image.tmdb.org/t/p/w500${selectedMovie.backdrop_path}` : "/placeholder.jpg"}
-          alt={selectedMovie.title || selectedMovie.name}
-          className="w-full h-32 object-cover rounded mb-3"
-        />
-        <p className="mb-3 text-gray-300 text-sm">{selectedMovie.overview}</p>
-        <p className="text-gray-300 text-sm">
-          Ano de Lançamento: {new Date(selectedMovie.release_date || selectedMovie.first_air_date || "").getFullYear()}
-        </p>
-        <p className="text-gray-300 text-sm">
-          País: {selectedMovie.production_countries?.map((country) => country.name).join(", ")}
-        </p>
-        <div className="flex items-center mt-2">
-          <p className="text-gray-300 text-sm mr-1">Classificação:</p>
-          {Array.from({ length: 5 }, (_, i) => (
-            <FaStar
-              key={i}
-              className={`text-yellow-500 text-xs ${i < Math.round((selectedMovie.vote_average || 0) / 2) ? "" : "opacity-25"}`}
-            />
-          ))}
-          <p className="text-gray-300 text-sm ml-1">({selectedMovie.vote_count} votos)</p>
-        </div>
-        <p className="text-gray-300 text-sm mt-2">
-          Gêneros: {selectedMovie.genres?.map((genre) => genre.name).join(", ")}
-        </p>
-        <p className="text-gray-300 text-sm mt-2">
-          Principais Atores: {selectedMovie.credits?.cast.slice(0, 3).map((actor) => actor.name).join(", ")}
-        </p>
-      </div>
+      {selectedMovie && (
+        <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center overflow-y-auto z-50">
+          <div className="bg-gray-900 p-4 rounded-md max-w-4xl mx-auto relative overflow-y-auto flex flex-col md:flex-row md:gap-6">
+            <button
+              className="absolute top-3 right-3 text-white bg-red-500 px-3 py-1 text-sm rounded"
+              onClick={() => setSelectedMovie(null)}
+            >
+              Fechar
+            </button>
+            <div className="flex-1">
+              <h2 className="text-xl font-bold text-white mb-3">
+                {selectedMovie.title || selectedMovie.name}
+              </h2>
+              <p className="text-gray-400 text-sm italic mb-2">
+                Tipo: {selectedMovie.first_air_date ? "Série" : "Filme"}
+              </p>
+              <img
+                src={
+                  selectedMovie.backdrop_path
+                    ? `https://image.tmdb.org/t/p/w500${selectedMovie.backdrop_path}`
+                    : "/placeholder.jpg"
+                }
+                alt={selectedMovie.title || selectedMovie.name}
+                className="w-full h-32 object-cover rounded mb-3"
+              />
+              <p className="mb-3 text-gray-300 text-sm">
+                {selectedMovie.overview}
+              </p>
+              <p className="text-gray-300 text-sm">
+                Ano de Lançamento:{" "}
+                {new Date(
+                  selectedMovie.release_date ||
+                    selectedMovie.first_air_date ||
+                    ""
+                ).getFullYear()}
+              </p>
+              <p className="text-gray-300 text-sm">
+                País:{" "}
+                {selectedMovie.production_countries
+                  ?.map((country) => country.name)
+                  .join(", ")}
+              </p>
+              <div className="flex items-center mt-2">
+                <p className="text-gray-300 text-sm mr-1">Classificação:</p>
+                {Array.from({ length: 5 }, (_, i) => (
+                  <FaStar
+                    key={i}
+                    className={`text-yellow-500 text-xs ${
+                      i < Math.round((selectedMovie.vote_average || 0) / 2)
+                        ? ""
+                        : "opacity-25"
+                    }`}
+                  />
+                ))}
+                <p className="text-gray-300 text-sm ml-1">
+                  ({selectedMovie.vote_count} votos)
+                </p>
+              </div>
+              <p className="text-gray-300 text-sm mt-2">
+                Gêneros:{" "}
+                {selectedMovie.genres?.map((genre) => genre.name).join(", ")}
+              </p>
+              <p className="text-gray-300 text-sm mt-2">
+                Principais Atores:{" "}
+                {selectedMovie.credits?.cast
+                  .slice(0, 3)
+                  .map((actor) => actor.name)
+                  .join(", ")}
+              </p>
+            </div>
 
-      {/* Trailer */}
-      <div className="flex-1 mt-4 md:mt-0">
-        {trailerId && (
-          <div className="w-full">
-            <YouTube videoId={trailerId} opts={{ width: "100%", height: "390" }} />
+            {/* Trailer */}
+            <div className="flex-1 mt-4 md:mt-0">
+              {trailerId && (
+                <div className="w-full">
+                  <YouTube
+                    videoId={trailerId}
+                    opts={{ width: "100%", height: "390" }}
+                  />
+                </div>
+              )}
+            </div>
           </div>
-        )}
-      </div>
-    </div>
-  </div>
-)}
-
+        </div>
+      )}
     </>
   );
 }
-
