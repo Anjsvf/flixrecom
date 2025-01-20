f4954d2f1acf4d9314c48ab709e964820f61c256
antonio santos <antoniosantos.devweb@gmail.com>
2025-01-20 19:29:10 -0300

    loading fixed

--- flixrecom/src/components/LoadingSpinner.tsx
+++ flixrecom/src/components/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingSpinner: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full min-h-screen bg-black">
+    <div className="flex flex-col items-center justify-center h-full min-h-screen ">
       <div className="relative w-24 h-24 mb-4">
         <div className="absolute w-full h-full rounded-full border-4 border-gray-700 border-t-4 border-t-red-600 animate-spin"></div>
         <div className="absolute w-full h-full flex items-center justify-center">
